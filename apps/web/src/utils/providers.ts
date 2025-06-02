3d0c4d21b8ed755a51087f66b026c5c7dc91005e
plaxswap <plaxswap@gmail.com>
2025-06-03 01:33:00 +0800

    update

--- apps/web/src/utils/providers.ts
+++
@@ -1,7 +0,0 @@
-import { StaticJsonRpcProvider } from '@ethersproject/providers'
-
-export const BSC_PROD_NODE = process.env.NEXT_PUBLIC_NODE_PRODUCTION || 'https://1rpc.io/37W31VzwTtHTisDEK/matic'
-
-export const bscRpcProvider = new StaticJsonRpcProvider(BSC_PROD_NODE)
-
-export default null
